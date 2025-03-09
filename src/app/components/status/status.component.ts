663d433547abd3d868eba0f35392cab4477f9cfc
unknown <giorgiigogua@gmail.com>
2025-03-09 09:52:49 -0700

    added status component

---
+++ src/app/components/status/status.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.component.html',
+  standalone: true,
+  imports: [CommonModule]
+})
+export class StatusComponent implements OnInit {
+
+  @Input() status: any;
+  @Input() text?: string
+  @Input() textColor?: string
+
+  public statusColor?: string;
+
+  ngOnInit(): void {
+    this.setStatusColor();
+  }
+
+  private setStatusColor(): void {
+    switch (this.status) {
+      case 'Ready to ship':
+        this.statusColor = '#b5cbbb';
+        this.textColor = 'green'
+        break;
+      case 'Cancelled':
+        this.statusColor = '#FF9999';
+        this.textColor = 'red'
+        break;
+      case 'In Testing':
+        this.statusColor = '#FFD580';
+        this.textColor = 'orange'
+        break;
+      case 'Backlog':
+        this.statusColor = '#CBD3D8';
+        this.textColor = 'gray'
+        break;
+    }
+  }
+
+}
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,13 +1,14 @@
-import { CommonModule, NgIf, NgIfContext } from '@angular/common';
-import { booleanAttribute, Component, Input, OnInit } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { booleanAttribute, Component, Input } from '@angular/core';
 import { DefaultTableService } from '../../services/default-table.service';
+import { StatusComponent } from '../status/status.component';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StatusComponent],
   providers: [DefaultTableService]
 
 })
