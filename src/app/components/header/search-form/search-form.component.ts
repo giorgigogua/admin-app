46ea4de931d694d43aea453967f6e35aa7b947f7
unknown <giorgiigogua@gmail.com>
2025-03-16 05:12:00 -0700

    header user

--- src/app/components/header/search-form/search-form.component.ts
+++ src/app/components/header/search-form/search-form.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrl: './search-form.component.scss',
   standalone:true
 })
 export class SearchFormComponent {
