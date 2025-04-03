52f9105189ba9b9328d302a43cc3d95fd77c59f1
GiorgiGogua <giorgiigogua@gmail.com>
2025-04-03 23:12:39 +0400

    mock pie chart

--- src/app/services/pie-chart.service.ts
+++ src/app/services/pie-chart.service.ts
@@ -1,15 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class PieChartService {
 
-  apiUrl = "http://localhost:3000/pie"
+  // apiUrl = "http://localhost:3000/pie"
+
+
+  mockPieData = [
+    {
+      "data": "44",
+      "id": "3f8f",
+      "backgroundColor": "rgb(15, 105, 165)"
+    },
+    {
+      "data": "55",
+      "id": "01f7",
+      "backgroundColor": "rgb(54, 162, 235)"
+    },
+    {
+      "data": "300",
+      "id": "e92c",
+      "backgroundColor": "rgb(83, 159, 209)"
+    },
+    {
+      "data": "300",
+      "id": "e92c",
+      "backgroundColor": "rgb(140, 197, 235)"
+    }
+  ]
 
   constructor(private httpClient: HttpClient) { }
 
   getPie(): Observable<any> {
-    return this.httpClient.get(this.apiUrl)
+    return of(this.mockPieData)
   }
 }
